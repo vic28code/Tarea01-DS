e213eb15158861231301620f670c0c79cc06cecb
Alejandro Sornoza <alesvera@espol.edu.ec>
2024-06-07 18:40:39 -0500

    Implementacion adapter

---
+++ TareaAspectos/src/seccionA/HdmiDevice.java
@@ -0,0 +1,11 @@
+package seccionA;
+
+public class HdmiDevice implements Hdmi{
+
+    @Override
+    public void connectWithHdmiCable() {
+        System.out.println("Conectando a cable HDMI");
+
+    }
+
+}
