4d145c77d7f31af590377d4ede27054fcce396a1
JoseM0lina <josmmoli@espol.edu.ec>
2024-06-07 19:03:51 -0500

    Seccion C por Jose Marin

--- TareaAspectos/src/seccionC/Main3.java
+++ TareaAspectos/src/seccionC/Main3.java
@@ -1,10 +1,22 @@
 package seccionC;
 
-public class Main3 {
+public class main3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) {
+	        RegistroEventos registro1 = RegistroEventos.obtenerInstancia();
+	        RegistroEventos registro2 = RegistroEventos.obtenerInstancia();
 
+	        registro1.registrar("Este es el primer mensaje de registro.");
+	        registro2.registrar("Este es el segundo mensaje de registro.");
+
+	        System.out.println("Hash de registro 1:" + registro1.hashCode());
+	        System.out.println("Hash de registro 2:" + registro2.hashCode());
+
+	        if (registro1 == registro2) {
+	            System.out.println("Ambos registros son la misma instancia.");
+	        } else {
+	            System.out.println("Los registros son instancias diferentes.");
+	        }
+	    }
 	}
 
-}
